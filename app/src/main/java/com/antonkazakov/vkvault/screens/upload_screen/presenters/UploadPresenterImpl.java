71b937b98586096e108ea4984950f489e2954e76
@Letnes <FireCat1987@gmail.com>
2016-09-24 01:38:29 +0300

    micro fix (all mime type)

--- app/src/main/java/com/antonkazakov/vkvault/screens/upload_screen/presenters/UploadPresenterImpl.java
+++ app/src/main/java/com/antonkazakov/vkvault/screens/upload_screen/presenters/UploadPresenterImpl.java
@@ -23,13 +23,12 @@ public class UploadPresenterImpl implements UploadPresenter {
 
     public void  chooseFile(){
         Intent intent = new Intent (Intent.ACTION_GET_CONTENT);
-        intent.setType("application/doc|application/docx|application/xls|application/xlsx|application/ppt|application/pptx|application/pdf|application/rtf|application/png|application/jpg|application/gif|application/psd|application/djvu|application/fb2");
+        intent.setType("*/*");
         intent.addCategory(Intent.CATEGORY_OPENABLE);
         intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION
                 | Intent.FLAG_GRANT_WRITE_URI_PERMISSION
                 | Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION);
-        intent.putExtra("browser_list_fontscale", "120%");
-        intent.putExtra("browser_list_layout", "2");
+
         mUploadView.showChooser(intent,"Выберите файл...",1);
     }
 }
